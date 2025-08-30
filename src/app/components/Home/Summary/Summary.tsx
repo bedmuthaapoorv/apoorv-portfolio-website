ec90f09c38bea641a71897236fa3431a998df396
Apoorv Bedmutha <bedmuthaapoorv@gmail.com>
2025-08-30 10:37:45 +0530

    created a seperate scss for typography, fixed missing space in summary

--- src/app/components/Home/Summary/Summary.tsx
+++ src/app/components/Home/Summary/Summary.tsx
@@ -4,10 +4,10 @@ export default function Summary() {
     <div className={styles.Summary}>
       <div className={styles.Summary__Container}>
         MTech with <strong>Honors from IIIT</strong> Gwalior,{" "}
-        <strong>2.3 years</strong>
-        professional experience in <strong>Backend</strong>,{" "}
-        <strong>Frontend</strong> and <strong>Full-Stack</strong> development
-        roles, <strong>SDE @ Nexus1 AI</strong>,{" "}
+        <strong>2.3 years</strong> professional experience in{" "}
+        <strong>Backend</strong>, <strong>Frontend</strong> and{" "}
+        <strong>Full-Stack</strong> development roles,{" "}
+        <strong>SDE @ Nexus1 AI</strong>,{" "}
         <strong>Top 16% coder globally</strong> @ Leetcode, GATE Qualified with{" "}
         <strong>All India Rank 8028</strong>
       </div>
