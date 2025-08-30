4c53cc9c2940d27a4fb46e36b6801daaca9d0e05
Apoorv Bedmutha <bedmuthaapoorv@gmail.com>
2025-08-30 10:53:14 +0530

    make summary responsive for mobile view

--- src/app/components/Home/Summary/Summary.tsx
+++ src/app/components/Home/Summary/Summary.tsx
@@ -1,8 +1,11 @@
 import styles from "./Summary.module.scss";
+import fontStyles from "@/app/resources/styles/Fonts.module.scss";
 export default function Summary() {
   return (
     <div className={styles.Summary}>
-      <div className={styles.Summary__Container}>
+      <div
+        className={`${styles.Summary__Container} ${fontStyles.primary} ${fontStyles.italic} ${fontStyles.paragraphL}`}
+      >
         MTech with <strong>Honors from IIIT</strong> Gwalior,{" "}
         <strong>2.3 years</strong> professional experience in{" "}
         <strong>Backend</strong>, <strong>Frontend</strong> and{" "}
